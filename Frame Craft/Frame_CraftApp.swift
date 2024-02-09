c4c0e1781c5a117eaae23e1f09dee55712816474
Andy <andyuan997@gmail.com>
2024-02-09 16:14:45 +0800

    Initial Commit

---
+++ Frame Craft/Frame_CraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frame_CraftApp.swift
+//  Frame Craft
+//
+//  Created by 袁維謙 on 2024/2/9.
+//
+
+import SwiftUI
+
+@main
+struct Frame_CraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

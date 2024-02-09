0724d860c069d8807209de30c1e7db55f89bbe01
Andy <andyuan997@gmail.com>
2024-02-09 16:22:56 +0800

    update

--- Frame Craft/ContentView.swift
+++ Frame Craft/ContentView.swift
@@ -5,20 +5,187 @@
 //  Created by 袁維謙 on 2024/2/9.
 //
 
+
 import SwiftUI
+import AppKit
 
 struct ContentView: View {
+    @State private var combinedImage: NSImage?
+    @State private var isImageDropped = false
+    var selectedOption1080x2340: String
+    var selectedOption1125x2436: String
+    var selectedOption1170x2532: String
+    var selectedOption1179x2556: String
+    var selectedOption1242x2688: String
+    var selectedOption1284x2778: String
+    var selectedOption1290x2796: String
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Drag and drop an image here.")
+                .font(.headline)
+            
+            if let combinedImage = combinedImage {
+                Image(nsImage: combinedImage)
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                Text(isImageDropped ? "Image Processing..." : "Wait for the picture to drag and drop here...")
+            }
         }
-        .padding()
+        .frame(width: 400, height: 300)
+        //.border(Color.gray, width: 1)
+        .onDrop(of: ["public.file-url"], isTargeted: nil) { providers in
+            isImageDropped = true // Immediately acknowledge the drop
+            loadDroppedImage(from: providers.first)
+            return true
+        }
     }
+
+    private func loadDroppedImage(from provider: NSItemProvider?) {
+        // 使用 _ 故意忽略返回的 NSProgress 對象
+        _ = provider?.loadObject(ofClass: URL.self) { url, error in
+            // 錯誤處理
+            if let error = error {
+                print("Error loading file: \(error.localizedDescription)")
+                return
+            }
+            guard let imageUrl = url else {
+                print("Invalid URL for dropped item.")
+                return
+            }
+            guard let userImage = NSImage(contentsOf: imageUrl) else {
+                print("Unable to create NSImage from URL: \(imageUrl)")
+                return
+            }
+            
+            
+            
+            DispatchQueue.main.async {
+                // 假如以設置 Assets.xcassets 且有圖片
+                
+                var screenshotframe = ""
+                switch userImage.size {
+                case CGSize(width: 1080, height: 2340), CGSize(width: 2340, height: 1080):
+                    screenshotframe = self.selectedOption1080x2340
+                case CGSize(width: 1125, height: 2436), CGSize(width: 2436, height: 1125):
+                    screenshotframe = self.selectedOption1125x2436
+                case CGSize(width: 1170, height: 2532), CGSize(width: 2532, height: 1170):
+                    screenshotframe = self.selectedOption1170x2532
+                case CGSize(width: 1179, height: 2556), CGSize(width: 2556, height: 1179):
+                    screenshotframe = self.selectedOption1179x2556
+                case CGSize(width: 1242, height: 2688), CGSize(width: 2688, height: 1242):
+                    screenshotframe = self.selectedOption1242x2688
+                case CGSize(width: 1284, height: 2778), CGSize(width: 2778, height: 1284):
+                    screenshotframe = self.selectedOption1284x2778
+                case CGSize(width: 1290, height: 2796), CGSize(width: 2796, height: 1290):
+                    screenshotframe = self.selectedOption1290x2796
+                default:
+                    let alert = NSAlert()
+                    alert.messageText = "Size Error"
+                    alert.informativeText = "The size of the image is not supported."
+                    alert.alertStyle = .warning
+                    alert.addButton(withTitle: "OK")
+                    alert.runModal()
+                    return
+                }
+                
+                
+                guard let frameImage = userImage.size.width < userImage.size.height ? NSImage(named: "\(screenshotframe) - Portrait") : NSImage(named: "\(screenshotframe) - Landscape") else {
+                    print("Failed to load iPhone frame image.")
+                    return
+                }
+                self.combinedImage = combineImages(frame: frameImage, screenshot: userImage)
+                
+                //以日期命名
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyyMMddHHmmss"
+                let dateString = dateFormatter.string(from: Date())
+                let fileName = "FrameShot_\(dateString).png"
+                
+                saveImage(self.combinedImage!, to: "SavedImages", with: fileName)
+            }
+        }
+    }
+
+    private func combineImages(frame: NSImage, screenshot: NSImage) -> NSImage {
+        guard let frameCGImage = frame.cgImage(forProposedRect: nil, context: nil, hints: nil),
+              let screenshotCGImage = screenshot.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+            print("Unable to convert NSImage to CGImage.")
+            return NSImage()
+        }
+        
+        let frameSize = CGSize(width: frameCGImage.width, height: frameCGImage.height)
+        print("iphone frame size：",frameSize)
+        let screenshotSize = CGSize(width: screenshotCGImage.width, height: screenshotCGImage.height)
+        print("input image size：",screenshotSize)
+        
+        let newImage = NSImage(size: frameSize)
+        newImage.lockFocus()
+        
+        
+        // 繪製iphone框架(作為圖像大小)
+        NSGraphicsContext.current?.cgContext.draw(frameCGImage, in: CGRect(origin: .zero, size: frameSize))
+        
+        // 計算置中
+        let targetX = (frameSize.width - screenshotSize.width) / 2
+        let targetY = (frameSize.height - screenshotSize.height) / 2
+        
+        // 輸入圖切圓角
+        let screenshotRect = CGRect(x: targetX, y: targetY, width: CGFloat(screenshotCGImage.width), height: CGFloat(screenshotCGImage.height))
+        let cornerRadius: CGFloat = 100 // 圓角半徑
+        let screenshotPath = CGPath(roundedRect: screenshotRect, cornerWidth: cornerRadius, cornerHeight: cornerRadius, transform: nil)
+        
+        // 圓角
+        NSGraphicsContext.current?.cgContext.addPath(screenshotPath)
+        NSGraphicsContext.current?.cgContext.clip()
+            
+        NSGraphicsContext.current?.cgContext.draw(screenshotCGImage, in: screenshotRect)
+        
+        // 繪製iphone框架(蓋上)
+        NSGraphicsContext.current?.cgContext.draw(frameCGImage, in: CGRect(origin: .zero, size: frameSize))
+
+        newImage.unlockFocus()
+        return newImage
+    }
+
+
+    
+    func saveImage(_ image: NSImage, to directory: String, with name: String) {
+        guard let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+            print("Failed to get CGImage from NSImage.")
+            return
+        }
+        
+        let bitmapRep = NSBitmapImageRep(cgImage: cgImage)
+        guard let pngData = bitmapRep.representation(using: .png, properties: [:]) else {
+            print("Failed to get PNG representation of NSImage.")
+            return
+        }
+        
+        let fileManager = FileManager.default
+        guard let directoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Failed to find the documents directory.")
+            return
+        }
+        
+        let fileURL = directoryURL
+            .appendingPathComponent(directory, isDirectory: true)
+            .appendingPathComponent(name)
+        
+        do {
+            try fileManager.createDirectory(at: fileURL.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: nil)
+            try pngData.write(to: fileURL)
+            print("Image was saved successfully to: \(fileURL.path)")
+            
+            // 打開finder 選中文件
+            NSWorkspace.shared.selectFile(fileURL.path, inFileViewerRootedAtPath: directoryURL.path)
+        } catch {
+            print("Failed to save image to disk: \(error)")
+        }
+    }
+
+
 }
 
-#Preview {
-    ContentView()
-}
+
--- Frame Craft/Frame_CraftApp.swift
+++ Frame Craft/Frame_CraftApp.swift
@@ -5,13 +5,118 @@
 //  Created by 袁維謙 on 2024/2/9.
 //
 
+
 import SwiftUI
 
 @main
 struct Frame_CraftApp: App {
+    @State private var selectedOption1080x2340: String = UserDefaults.standard.string(forKey: "SelectedOption1080x2340") ?? "iPhone 13 mini - Midnight"
+    @State private var selectedOption1125x2436: String = "iPhone 11 Pro"
+    @State private var selectedOption1170x2532: String = "iPhone 14 - Purple"
+    @State private var selectedOption1179x2556: String = "iPhone 14 Pro - Deep Purple"
+    @State private var selectedOption1242x2688: String = "iPhone 11"
+    @State private var selectedOption1284x2778: String = "iPhone 13 Pro Max - Graphite"
+    @State private var selectedOption1290x2796: String = "iPhone 15 Pro Max - Black Titanium"
+    @State private var language: String = "English"
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(selectedOption1080x2340: selectedOption1080x2340, selectedOption1125x2436: selectedOption1125x2436, selectedOption1170x2532: selectedOption1170x2532, selectedOption1179x2556: selectedOption1179x2556, selectedOption1242x2688: selectedOption1242x2688, selectedOption1284x2778: selectedOption1284x2778, selectedOption1290x2796: selectedOption1290x2796)
+        }
+        .commands {
+            CustomMenu(selectedOption1080x2340: $selectedOption1080x2340, selectedOption1125x2436: $selectedOption1125x2436, selectedOption1170x2532: $selectedOption1170x2532, selectedOption1179x2556: $selectedOption1179x2556, selectedOption1242x2688: $selectedOption1242x2688, selectedOption1284x2778: $selectedOption1284x2778, selectedOption1290x2796: $selectedOption1290x2796, language: $language)
         }
     }
+}
+
+struct CustomMenu: Commands {
+    @Binding var selectedOption1080x2340: String
+    @Binding var selectedOption1125x2436: String
+    @Binding var selectedOption1170x2532: String
+    @Binding var selectedOption1179x2556: String
+    @Binding var selectedOption1242x2688: String
+    @Binding var selectedOption1284x2778: String
+    @Binding var selectedOption1290x2796: String
+    @Binding var language: String
+    
+    var body: some Commands {
+        CommandMenu("Custom Style") {
+            Picker("5.8\" 1080x2340", selection: $selectedOption1080x2340) {
+                            Text("iPhone 12 mini - White").tag("iPhone 12 mini - White")
+                            Text("iPhone 13 mini - Midnight").tag("iPhone 13 mini - Midnight")
+                            Text("iPhone 13 mini - Starlight").tag("iPhone 13 mini - Starlight")
+
+                        }
+            Picker("5.8\" 1125x2436", selection: $selectedOption1125x2436) {
+                            Text("iPhone 11 Pro").tag("iPhone 11 Pro")
+                        }
+            Picker("5.8\" 1170x2532", selection: $selectedOption1170x2532) {
+                            Text("iPhone 12 - White").tag("iPhone 12 - White")
+                            Text("iPhone 12 Pro - Silver").tag("iPhone 12 Pro - Silver")
+                            Text("iPhone 13 - Midnight").tag("iPhone 13 - Midnight")
+                            Text("iPhone 13 - Starlight").tag("iPhone 13 - Starlight")
+                            Text("iPhone 13 Pro - Graphite").tag("iPhone 13 Pro - Graphite")
+                            Text("iPhone 13 Pro - Silver").tag("iPhone 13 Pro - Silver")
+                            Text("iPhone 14 - Blue").tag("iPhone 14 - Blue")
+                            Text("iPhone 14 - Midnight").tag("iPhone 14 - Midnight")
+                            Text("iPhone 14 - Purple").tag("iPhone 14 - Purple")
+                            Text("iPhone 14 - Red").tag("iPhone 14 - Red")
+                            Text("iPhone 14 - Starlight").tag("iPhone 14 - Starlight")
+                            Text("iPhone 14 Plus - Blue").tag("iPhone 14 Plus - Blue")
+                            Text("iPhone 14 Plus - Midnight").tag("iPhone 14 Plus - Midnight")
+                            Text("iPhone 14 Plus - Purple").tag("iPhone 14 Plus - Purple")
+                            Text("iPhone 14 Plus - Red").tag("iPhone 14 Plus - Red")
+                            Text("iPhone 14 Plus - Starlight").tag("iPhone 14 Plus - Starlight")
+                        }
+            Picker("6.1\" 1179x2556", selection: $selectedOption1179x2556) {
+                            Text("iPhone 14 Pro - Deep Purple").tag("iPhone 14 Pro - Deep Purple")
+                            Text("iPhone 14 Pro - Gold").tag("iPhone 14 Pro - Gold")
+                            Text("iPhone 14 Pro - Silver").tag("iPhone 14 Pro - Silver")
+                            Text("iPhone 14 Pro - Space Black").tag("iPhone 14 Pro - Space Black")
+                            Text("iPhone 15 - Black").tag("iPhone 15 - Black")
+                            Text("iPhone 15 - Blue").tag("iPhone 15 - Blue")
+                            Text("iPhone 15 - Green").tag("iPhone 15 - Green")
+                            Text("iPhone 15 - Pink").tag("iPhone 15 - Pink")
+                            Text("iPhone 15 - Yellow").tag("iPhone 15 - Yello")
+                            Text("iPhone 15 Pro - Black Titanium").tag("iPhone 15 Pro - Black Titanium")
+                            Text("iPhone 15 Pro - Blue Titanium").tag("iPhone 15 Pro - Blue Titanium")
+                            Text("iPhone 15 Pro - Natural Titanium").tag("iPhone 15 Pro - Natural Titanium")
+                            Text("iPhone 15 Pro - White Titanium").tag("iPhone 15 Pro - White Titanium")
+                        }
+            Picker("6.5\" 1242x2688", selection: $selectedOption1242x2688) {
+                            Text("iPhone 11").tag("iPhone 11")
+                            Text("iPhone 11 Pro Max").tag("iPhone 11 Pro Max")
+                            
+                        }
+            Picker("6.5\" 1284x2778", selection: $selectedOption1284x2778) {
+                            Text("iPhone 12 Pro Max - Silver").tag("iPhone 12 Pro Max - Silver")
+                            Text("iPhone 13 Pro Max - Graphite").tag("iPhone 13 Pro Max - Graphite")
+                            Text("iPhone 13 Pro Max - Silver").tag("iPhone 13 Pro Max - Silver")
+                        }
+            Picker("6.7\" 1290x2796", selection: $selectedOption1290x2796) {
+                            Text("iPhone 14 Pro Max - Deep Purple").tag("iPhone 14 Pro Max - Deep Purple")
+                            Text("iPhone 14 Pro Max - Gold").tag("iPhone 14 Pro Max - Gold")
+                            Text("iPhone 14 Pro Max - Silver").tag("iPhone 14 Pro Max - Silver")
+                            Text("iPhone 14 Pro Max - Space Black").tag("iPhone 14 Pro Max - Space Black")
+                            Text("iPhone 15 Plus - Black").tag("iPhone 15 Plus - Black")
+                            Text("iPhone 15 Plus - Blue").tag("iPhone 15 Plus - Blue")
+                            Text("iPhone 15 Plus - Green").tag("iPhone 15 Plus - Green")
+                            Text("iPhone 15 Plus - Pink").tag("iPhone 15 Plus - Pink")
+                            Text("iPhone 15 Plus - Yellow").tag("iPhone 15 Plus - Yellow")
+                            Text("iPhone 15 Pro Max - Black Titanium").tag("iPhone 15 Pro Max - Black Titanium")
+                            Text("iPhone 15 Pro Max - Blue Titanium").tag("iPhone 15 Pro Max - Blue Titanium")
+                            Text("iPhone 15 Pro Max - Natural Titanium").tag("iPhone 15 Pro Max - Natural Titanium")
+                            Text("iPhone 15 Pro Max - White Titanium").tag("iPhone 15 Pro Max - White Titanium")
+                        }
+        }
+
+        CommandMenu("Setting") {
+            Picker("Language", selection: $language) {
+                            Text("English").tag("English")
+                            //Text("繁體中文").tag("繁體中文")
+                            //Text("简体中文").tag("简体中文")
+            }
+        }
+    }
+    
 }
